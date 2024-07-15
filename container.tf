f682b92832be8c2789229f649765ee2a5ef5988a
Saisreekar9666 <gsaisreekar9666@gmail.com>
2024-07-15 16:49:12 +0530

    ecomm container

--- container.tf
+++ container.tf
@@ -1,5 +1,5 @@
 resource "docker_container" "nginx_container" {
-  image = docker_image.nginx.image_id
+  image = docker_image.gsaisreekar9666/ecomm.image_id
   name  = "n1"
    ports{
     internal= 80
