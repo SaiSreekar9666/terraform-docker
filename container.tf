c8be75d1b6d4927345acdea39653f3c31f5df2da
Saisreekar9666 <gsaisreekar9666@gmail.com>
2024-07-15 15:06:21 +0530

    error fixing

--- container.tf
+++ container.tf
@@ -1,8 +1,8 @@
 resource "docker_container" "nginx_container" {
   image = docker_image.nginx.image_id
   name  = "n1"
-   port {
-      port        = 80
-      target_port = 80
+   port{
+    internal= 80
+    external = 80
     }
 }
