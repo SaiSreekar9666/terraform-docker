61ac7b8b09b356d96a541b1c2405b31dc4f60f8d
Saisreekar9666 <gsaisreekar9666@gmail.com>
2024-07-15 17:04:30 +0530

    ecomm

--- image.tf
+++ image.tf
@@ -2,3 +2,14 @@
 resource "docker_image" "ecomm" {
   name = "gsaisreekar9666/ecomm"
 }
+#build the image
+resource "docker_image" "ecomm" {
+  name = "ecomm"
+  build {
+    context = "."
+    tag     = ["gsaisreekar9666/ecomm1"]
+    label = {
+      author : "sai"
+    }
+  }
+}
