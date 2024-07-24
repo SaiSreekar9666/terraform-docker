b60707c363d0b7ee026f6360071bd4f5830e98ee
saisreekar9666 <gsaisreekar9666@gmail.com>
2024-07-24 17:39:26 +0530

    chNGES

--- provider.tf
+++ provider.tf
@@ -10,6 +10,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  access_key = aws_access_key
-  secret_key = aws_secret_key
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
