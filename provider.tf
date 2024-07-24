39fbd59e20a732ce537437c9d7619139571b9847
saisreekar9666 <gsaisreekar9666@gmail.com>
2024-07-24 17:37:58 +0530

    providers changes

--- provider.tf
+++ provider.tf
@@ -1,12 +1,15 @@
 terraform {
   required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "3.0.2"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
 
-provider "docker" {
-  host = "unix:///var/run/docker.sock"
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  access_key = aws_access_key
+  secret_key = aws_secret_key
 }
