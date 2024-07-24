af612d14353c7d59ed65a484eea4fbe361779545
saisreekar9666 <gsaisreekar9666@gmail.com>
2024-07-24 18:06:26 +0530

    changes

---
+++ variable.tf
@@ -0,0 +1,10 @@
+variable "aws_access_key" {
+    description = "value of the access-key"
+    type = string
+  
+}
+variable "aws_secret_key" {
+    description = "value of the secret-key"
+    type = string
+  
+}
